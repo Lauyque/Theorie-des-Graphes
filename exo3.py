fec0d0f7b2896d68a00b718d76b62c189a21be53
LE DOHER Loïc <91799630+Lauyque@users.noreply.github.com>
2023-12-22 16:06:04 +0100

    exo 3

---
+++ exo3.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from numpy import random
+
+def creer_graphe_aleatoire():
+    nb_sommets = random.randint(2, 16)
+    G = nx.Graph()
+    
+    for i in range(nb_sommets):
+        G.add_node(i)
+    
+    for i in range(nb_sommets):
+        for j in range(i+1, nb_sommets):
+            if random.rand() > 0.5:
+                G.add_edge(i, j)
+    
+    return G
+
+G_aleatoire = creer_graphe_aleatoire()
+pos = nx.spring_layout(G_aleatoire)
+nx.draw_networkx_nodes(G_aleatoire, pos, node_color='blue', node_size=500)
+nx.draw_networkx_edges(G_aleatoire, pos)
+nx.draw_networkx_labels(G_aleatoire, pos, font_color='white')
+plt.axis('off')
+plt.show()
